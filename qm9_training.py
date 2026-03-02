d9bcc40971f51f071663b2619081e0ef52c6067c
Kevin Rojas <kevinrojas99@gmail.com>
2026-03-02 16:10:28 -0500

    Fixed typo

--- qm9_training.py
+++ qm9_training.py
@@ -110,7 +110,7 @@ def training(**opts):
     muon_params = param_groups['muon_params']
     adam_params = param_groups['adam_params']
     if opts.optimizer == 'muon':
-        muon_opt = torch.optim.Muon(muon_params, lr=0.02, weight_decay=0.01, adjust_lr="match_rms_adamw")
+        muon_opt = torch.optim.Muon(muon_params, lr=0.02, weight_decay=0.01, adjust_lr_fn="match_rms_adamw")
         adam_opt = torch.optim.AdamW(adam_params, lr=opts.lr)
         opt = CombinedOptimizer(muon_opt, adam_opt)
     elif opts.optimizer == 'adamw':
